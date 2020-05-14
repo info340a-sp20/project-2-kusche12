77cfe5ece1347380b4405fdb7b3c0e5d87fe22ae
Kyle Kusche <kusche12@uw.edu>
2020-05-13 23:14:42 -0700

    about to lift the state

--- src/index.js
+++ src/index.js
@@ -113,14 +113,24 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: [],
+      // All cards, initialized with one blank
+      cards: [{question: "", 
+               answers: [{text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}]}],
+      cardPosition: 0
     };
+  }
+
+  addCard = () => {
+    let oldCards = arrayClone(this.state.cards);
+    let newCards = oldCards.slice(0, this.cardPosition);
+    newCards.push({question: "", answers: [{text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}]}); // add a blank card below
+    newCards.push(...oldCards.slice(this.cardPosition, oldCards.length));
   }
 
   render() {
     return (
       <div>
-        <MakerSpace/>
+        <MakerSpace addCard={this.addCard} cardPosition={this.cardPosition}/>
       </div>
     );
   }
