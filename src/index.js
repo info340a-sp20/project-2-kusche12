916cf1452cb4b241cde4d09d6d2a67434643b349
Kyle Kusche <kusche12@uw.edu>
2020-05-15 13:58:56 -0700

    Getting the maker space buttons to work

--- src/index.js
+++ src/index.js
@@ -8,24 +8,50 @@ class App extends React.Component {
     super(props);
     this.state = {
       // All cards, initialized with one blank
-      cards: [{question: "", 
-               answers: [{text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}]}],
-      cardPosition: 0
+      cards: [{question: "Is the sky blue?", 
+               answers: [{text: 'Yes', isCorrect: true}, {text: 'No', isCorrect: false}, {text: 'Sometimes', isCorrect: false}, {text: 'Maybe', isCorrect: false}]}],
+      cardPosition: 0,
     };
   }
 
   // add a blank card below current card
-  addCard = () => {
+  addCard = (event) => {
+    event.preventDefault();
+
     let oldCards = arrayClone(this.state.cards);
-    let newCards = oldCards.slice(0, this.cardPosition);
-    newCards.push({question: "", answers: [{text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}]}); 
-    newCards.push(...oldCards.slice(this.cardPosition, oldCards.length));
+    // return all cards from beginning to current
+    let newCards = oldCards.slice(0, this.state.cardPosition + 1);
+    // append a new blank card
+    newCards.push({question: "", answers: [{text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}, {text: '', isCorrect: false}]});
+    // append the remaining cards
+    newCards.push(...oldCards.slice(this.state.cardPosition + 1, oldCards.length));
+
+    this.setState({cards: newCards}); 
+  }
+
+  moveCard = (event) => {
+    event.preventDefault();
+    if (event.target.value === 'forward' && this.state.cardPosition + 1 < this.state.cards.length) {
+      this.setState({cardPosition: this.state.cardPosition + 1})
+    } else if (event.target.value === 'back' && this.state.cardPosition > 0) {
+      this.setState({cardPosition: this.state.cardPosition - 1})
+    }
+    // UPDATE THE MAKER SPACE HERE
+  }
+
+  deleteCard = (event) => {
+    event.preventDefault();
   }
 
   render() {
     return (
       <div>
-        <MakerSpace addCard={this.addCard} cardPosition={this.cardPosition}/>
+        <MakerSpace 
+          addCard={this.addCard} 
+          moveCard={this.moveCard} 
+          deleteCard={this.deleteCard} 
+          cardPosition={this.state.cardPosition} 
+          cards={this.state.cards}/>
       </div>
     );
   }
