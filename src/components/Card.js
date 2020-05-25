6385a5c057a9d7045a3405afad7f7283140c965e
Kyle Kusche <kusche12@uw.edu>
2020-05-24 21:45:53 -0700

    Maker Space is fully responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
+// back and forward buttons have their own function because there was an error when you would click on the fontawesome
+// if you can think of a better solution, please implement it haha
 
 class Card extends React.Component {
+    backArrow = (event) => {
+      event.target.value = 'prev';
+      this.props.moveCardHandler(event);
+    }
+    forwardArrow = (event) => {
+      event.target.value = 'next';
+      this.props.moveCardHandler(event);
+    }
+
     render() {
       let answerTextRender = [];
       let updatedAnswers = this.props.answers;
       
       // THIS DOES NOT WORK. IT SHOULD SHOW A DEFAULT ____ WHEN THERE IS NO ANSWER PROVIDED
       for (let i = 0; i < updatedAnswers.length; i++) {
-        answerTextRender.push(<h4 key={i}>{updatedAnswers[i]}</h4>);    // User made Answer
+        answerTextRender.push(<h5 key={i}>{updatedAnswers[i]}</h5>);    // User made Answer
       }
     
       return (
-        <div className='card-title-cover'>
+        <div className='card-total-cover'>
           <h3>Question {this.props.questionNumber + 1}</h3>
-          <div className="card">
-            <div className="card-question-cover">
-              <h2 className="card-question">{this.props.question}</h2>
+          <div className="card-buttons-cover">
+            <button value='prev' onClick={this.props.moveCardHandler}><FontAwesomeIcon onClick={this.backArrow} icon={faChevronLeft} size="3x"></FontAwesomeIcon></button>
+            <div className="card">
+              <div className="card-question-cover">
+                <h4>{this.props.question}</h4>
+              </div>
+              <div className="card-answer-group">
+                {answerTextRender}
+              </div>
             </div>
-            <div className="card-answer-group">
-              {answerTextRender}
-            </div>
+            <button value='next' onClick={this.props.moveCardHandler}>
+              <FontAwesomeIcon onClick={this.forwardArrow} icon={faChevronRight} size="3x"></FontAwesomeIcon>
+            </button>
           </div>
+          <button className="card-submit" value='submit' onClick={this.props.submitQuiz}>Submit</button>
         </div>
       )
     }
--- src/components/ErrorAlert.js
+++ src/components/ErrorAlert.js
@@ -3,8 +3,26 @@ import './index.css';
 
 class ErrorAlert extends React.Component {
     render() {
+        let display = [];
+        this.props.errorcode.forEach((error, i) => {
+            if (error && i === 0) {
+                display.push(
+                    <div className="error-block" key={i}><p>Remember to add a question</p></div>
+                );
+            } else if (error && i === 1) {
+                display.push(
+                    <div className="error-block" key={i}><p>You must have at least two (2) answer choices</p></div>
+                );
+            } else if (error && i === 2) {
+                display.push(
+                    <div className="error-block" key={i}><p>Don't forget to add in a correct answer choice</p></div>
+                );
+            }
+        })
         return (
-            <h1>Hello</h1>
+            <div className="error-list">
+                {display}
+            </div>
         );
     }
 }
